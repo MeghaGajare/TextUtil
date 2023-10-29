9405c8b45ca30aecc804b73d74321fd6ff558271
Megha Gajare <gajarecmegha19@gmail.com>
2023-10-30 00:44:44 +0530

    TextUtils code added

---
+++ textutils/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar.js';
+
+function App() {
+  return (
+    <div>
+       <NavBar/>
+        
+    </div>
+  );
+}
+
+export default App;
